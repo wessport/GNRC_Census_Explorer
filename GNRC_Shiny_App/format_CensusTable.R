795d8188632ec1706a1a87ab38bf689acdcaf728
Wes Porter <wesporter92@gmail.com>
2018-05-09 15:59:24 -0500

    Added scratchpad file to repo incase comp dies again

--- GNRC_Shiny_App/format_CensusTable.R
+++ GNRC_Shiny_App/format_CensusTable.R
@@ -3,7 +3,7 @@
 # Creation Date: 07-MAY-2018
 # Script Author: Wes Porter
 # Script Summary: Function for formatting tidycensus data
-# Last Updated: 08-MAY-2018
+# Last Updated: 09-MAY-2018
 
 library(tidycensus)
 library(magrittr)
@@ -11,34 +11,47 @@ library(sf)
 library(tidyverse)
 library(lettercase)
 library(lwgeom)
+library(stringr)
+
+# Currently works for ACS 2016-2015
+# Will need to update to handle 2014-2011
 
 census_api_key(Sys.getenv("CENSUS_API_KEY"))
 
-format_Census <- function(state, counties, geo, vars){
+state = 'TN'
+counties <- c('Cheatham','Davidson','Dickson','Houston','Humphreys','Montgomery','Maury','Robertson','Rutherford','Stewart','Sumner',
+              'Trousdale','Williamson','Wilson')
+geo <- 'tract'
+tableID <- 'B25063'
+yr <- 2016
+
+format_Census <- function(state, counties, geo, tableID, yr){
 
   if (is.na(counties)){
-    
+
     # Request data from Census API
-    census_data <- get_acs(state = state, geography = geo, 
-                           variables = vars, geometry = TRUE)
-    
+    census_data <- get_acs(state = state, geography = geo,
+                           table = tableID, geometry = TRUE)
+
   } else {
-    
+
     # Request data from Census API
     census_data <- get_acs(state = state, county = counties, geography = geo, 
-                    variables = vars, geometry = TRUE)
-  }
-  
+                    table = tableID, year = yr, geometry = TRUE)
+}
+
   # Complete list of variable names and their respective IDs
-  svar <- read.csv("C:/Users/wPorter/Data/Census/ACS5_tabular_data/data_dictionary/selected_variableID.csv")
+  acs_variables <-  load_variables(yr, "acs5", cache = FALSE)
+  acs_variables %>% mutate(table_name = str_split_fixed(name, "_", 2)[,1]) -> acs_variables
   
   # Subset variable names based on variables of interest 
-  a <- svar[svar$name %in% vars,]
+  a <- acs_variables[acs_variables$table_name %in% tableID,]
   
   # Prep variable IDs for joining
   a %>% mutate(name_mod = substr(name,1,nchar(as.character(name))-1))%>% 
     mutate(label_mod = substr(label,11,nchar(as.character(label)))) %>% 
-    mutate(label_mod = gsub("!!"," ",label_mod)) -> a
+    mutate(label_mod = gsub("!!"," ",label_mod)) %>%
+    select(-table_name) -> a
   
   # Join variable names and variable IDs together
   # Be careful to preserve sf class for the geometry attribute
@@ -62,5 +75,69 @@ format_Census <- function(state, counties, geo, vars){
   
   return(census_format)
 }
+
+test <-  format_Census(state, counties, geo, tableID, 2015)
+
+
+# 2011-2014 ------------------------------------------------------------
+yr <- 2014
+
+# Request data from Census API
+census_data <- get_acs(state = state, county = counties, geography = geo, 
+                       table = tableID, year = yr, geometry = TRUE)
+
+# Complete list of variable names and their respective IDs
+acs_variables <-  load_variables(yr, "acs5", cache = FALSE)
+acs_variables %>% mutate(table_name = str_split_fixed(name, "_", 2)[,1]) -> acs_variables
+
+# Subset variable names based on variables of interest 
+b <- acs_variables[acs_variables$table_name %in% tableID,]
+
+b %>% 
+  filter(grepl("*E$",b$name)) %>%
+  select(-table_name) %>%
+  mutate(label = gsub(":!!"," ",label)) %>%
+  mutate(label = gsub(":"," ",label)) %>%
+  mutate(name_mod = substr(name,1,nchar(as.character(name))-1)) -> b
+
+a %>% 
+  mutate(label_mod = str_split_fixed(label,"!!",2))
+
+t <- cbind.data.frame(a, c('a','b','c') = str_split_fixed(a$label,"!!",3))
+
+# NEED TO FIGURE OUT HOWTO SPLIT AND THEN ADD IN TOTAL 
+t %>%
+  mutate(t)
+
+# Prep variable IDs for joining
+# a %>% mutate(name_mod = substr(name,1,nchar(as.character(name))-1))%>% 
+#   mutate(label_mod = substr(label,11,nchar(as.character(label)))) %>% 
+#   mutate(label_mod = gsub("!!"," ",label_mod)) %>%
+#   select(-table_name) -> a
+
+# Join variable names and variable IDs together
+# Be careful to preserve sf class for the geometry attribute
+census_data %>% 
+  left_join(a, by = c("variable" = "name_mod")) %>% 
+  select(-name,-concept)-> census_data
+
+# Formatting Census table in familiar format for users
+# Long to wide format with variable names
+census_data %>% 
+  gather(measurement, value, estimate, moe) %>%
+  unite(variable_combined, label, measurement, sep = " ") %>%
+  group_by(NAME) %>%
+  arrange(GEOID,variable) %>%
+  select(-variable) %>%
+  spread(variable_combined, value) %>%
+  mutate(Area_m2 = st_area(geometry)) -> census_format
+
+# Reproject to WGS 84 EPSG 4326 A.K.A. Google's Projection
+census_format <- st_transform(census_format, 4326, use_gdal = T)
+  
+
+
+
+
 
 
---
+++ GNRC_Shiny_App/scratch.r
@@ -0,0 +1,180 @@
+# A sandbox for working with census data
+library(tidycensus)
+library(leaflet)
+library(rgdal)
+library(magrittr)
+library(sf)
+library(tigris)
+library(tidyverse)
+library(lettercase)
+options(tigris_class = "sf")
+options(tigris_use_cache = TRUE)
+census_api_key("6999d8d1e472e95e754d605f9a5646beec7eede5", install = TRUE)
+Sys.getenv("CENSUS_API_KEY")
+
+path = "C:/Users/wPorter/Data/Census/census_shapefiles/GNR/counties"
+
+gnr_2017 <- readOGR(path, layer = 'cb_2017_gnr_county')
+
+plot(gnr_2017)
+
+test <-  st_read(path, layer = 'cb_2017_gnr_county')
+
+
+plot(test$NAME, test$ALAND)
+
+barplot(test$ALAND, main="Area Distribution", 
+        names.arg=test$NAME)
+
+
+labels <- test$NAME
+
+c <- paste("<strong>", labels, "</strong>", sep ='')
+
+leaflet(test) %>%
+  addProviderTiles(providers$Stamen.TonerLite,
+                   options = providerTileOptions(noWrap =TRUE)
+  ) %>%
+  addPolygons(fillColor = ~colorQuantile("YlOrRd", ALAND)(ALAND), 
+              fillOpacity = 0.5, 
+              weight = 2, 
+              stroke = T, 
+              color = "grey", 
+              opacity = 1,
+              dashArray = "3",
+              highlight = highlightOptions(color = "white", weight = 3, bringToFront = TRUE),
+              #label = labels
+              label = lapply(c,HTML)
+              )
+
+
+lableOptions = labelOptions(
+  style = list("font-weight" = "normal", padding = "3px 8px"),
+  textsize = "15px",
+  direction = "auto")
+
+
+# Testing to see which geographies Tidycensus supports
+
+# COUNTY
+# county
+# tract
+# block group
+
+# STATE 
+# place - no geometry via tidycensus
+# combined statistical area
+
+# NATION
+# urban area
+
+tn <- get_acs(geography = "place", 
+              variables = c(medincome = "B19013_001"), 
+              state = "TN")
+
+tn <- get_acs(geography = "county", variables = c(medincome = "B19013_001", medincome2 = "B19013_002"), state = "TN", endyear = 2015)
+
+nation <- get_acs(geography = 'urban area', variables = c(medincome = "B19013_001"))
+
+
+acs_variables_16 <-  load_variables(2016, "acs5", cache = TRUE)
+acs_variables_15 <-  load_variables(2015, "acs5", cache = TRUE)
+acs_variables_14 <-  load_variables(2014, "acs5", cache = TRUE)
+acs_variables_13 <-  load_variables(2013, "acs5", cache = TRUE)
+acs_variables_12 <-  load_variables(2012, "acs5", cache = TRUE)
+acs_variables_11 <-  load_variables(2011, "acs5", cache = TRUE)
+
+
+v <- read.csv("C:/Users/wPorter/Data/Census/ACS5_tabular_data/data_dictionary/variables.csv", stringsAsFactors = FALSE)
+
+write.table(unique(v),"C:/Users/wPorter/Data/Census/ACS5_tabular_data/data_dictionary/unique_variables.csv", sep=",", col.names = T, row.names = F)
+
+nrow(unique(v))
+
+test <- get_acs(state = "TN", county = c("Davidson","Rutherford"), geography = "tract", 
+                  variables = "B19013_001", geometry = TRUE)
+
+
+gross_rent <- c('B25063_001E','B25063_002E','B25063_003E','B25063_004E','B25063_005E','B25063_006E','B25063_007E','B25063_008E','B25063_009E',
+                'B25063_010E','B25063_011E','B25063_012E','B25063_013E','B25063_014E','B25063_015E','B25063_016E','B25063_017E','B25063_018E',
+                'B25063_019E','B25063_020E','B25063_021E','B25063_022E','B25063_023E','B25063_024E','B25063_025E','B25063_026E','B25063_027E')
+
+gsub("E", "",gross_rent) -> gross_rent
+
+test <- get_acs(table = "B25063", state = "TN", county = c("Davidson", "Rutherford"), geography = "tract",geometry = TRUE, year = 2017)
+
+# Formatting Census table in familiar format for users
+# * Still need to figure out how to add variable names elegantly
+test %>% 
+  gather(measurement, value, estimate, moe) %>% 
+  unite(variable_combined, variable, measurement )%>%
+  group_by(NAME) %>%
+  arrange(GEOID,variable_combined)%>%
+  spread(variable_combined, value)-> t
+
+
+# Leaflet works with wide format, expects only 1 variable at a time
+leaflet(t) %>%
+  addProviderTiles(providers$Stamen.TonerLite,
+                   options = providerTileOptions(noWrap =TRUE)
+  ) %>%
+  addPolygons(fillColor = ~colorQuantile("YlOrRd", B25063_001_estimate)(B25063_001_estimate), 
+              fillOpacity = 0.5, 
+              weight = 2, 
+              stroke = T, 
+              color = "grey", 
+              opacity = 1,
+              dashArray = "3",
+              highlight = highlightOptions(color = "white", weight = 3, bringToFront = TRUE)
+  )
+
+# Reproject to WGS 84 EPSG 4326 A.K.A. Google's Projection
+t <- st_transform(t, 4326, use_gdal = T)
+
+
+svar <- read.csv("C:/Users/wPorter/Data/Census/ACS5_tabular_data/data_dictionary/selected_variableID.csv")
+
+a <- svar[svar$name %in% gross_rent,]
+
+
+a %>% mutate(name_mod = substr(name,1,nchar(as.character(name))-1)) -> a
+
+# Handle upper case
+str_lower("GROSS RENT") %>% str_title_case()
+
+a %>% mutate(label_mod = substr(label,11,nchar(as.character(label)))) %>% 
+  mutate(label_mod = gsub("!!"," ",label_mod)) %>%
+  mutate(label_mod = paste(str_title_case(str_lower(concept)), label_mod, sep =" ")) -> a
+
+colnames(t)
+
+col_labels <- colnames(t)
+
+
+
+##########
+
+gross_rent <- c('B25063_001E','B25063_002E','B25063_003E','B25063_004E','B25063_005E','B25063_006E','B25063_007E','B25063_008E','B25063_009E',
+                'B25063_010E','B25063_011E','B25063_012E','B25063_013E','B25063_014E','B25063_015E','B25063_016E','B25063_017E','B25063_018E',
+                'B25063_019E','B25063_020E','B25063_021E','B25063_022E','B25063_023E','B25063_024E','B25063_025E','B25063_026E','B25063_027E')
+
+test <- get_acs(state = "TN", county = c("Rutherford"), geography = "tract", 
+                variables = gross_rent, geometry = TRUE, year = 2014)
+
+
+a %>% select(name_mod, label_mod) %>%
+  right_join(test, by = c("name_mod" = "variable")) -> test2
+
+
+# Formatting Census table in familiar format for users
+test2 %>% 
+  gather(measurement, value, estimate, moe) %>%
+  unite(variable_combined, label_mod, measurement, sep = " ") %>%
+  group_by(NAME) %>%
+  arrange(GEOID,name_mod) %>%
+  select(-name_mod) %>%
+  spread(variable_combined, value)-> t2
+
+
+
+
